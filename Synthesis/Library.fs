510c6565deb0f7d30fafb5f7e82cb5ecfc932be9
Marc <g18m6731@campus.ru.ac.za>
2020-03-09 20:28:00 +0200

    Updated complete up to Q8

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -1,31 +1,48 @@
 ﻿module Synthesis
 
-let abelar _ =
-    failwith "Not implemented"
+let abelar x = 
+    if x>12 && x<3097 && x % 12 = 0 then true else false
 
-let area _ _ =
-    failwith "Not implemented"
+let area b h =
+    if b<0.0 || h<0.0 then 
+        failwith "Negative base or height"
+    else 0.5*b*h
+        
 
-let zollo _ =
-    failwith "Not implemented"
+let zollo x =
+    if (x<0) then -1*x
+    else x*2
 
-let min _ _ =
-    failwith "Not implemented"
 
-let max _ _ =
-    failwith "Not implemented"
+let min x y =
+    if x<y then x
+    else y
 
-let ofTime _ _ _ =
-    failwith "Not implemented"
+let max x y =
+    if x<y then y
+    else x
 
-let toTime _ =
-    failwith "Not implemented"
+let ofTime h m s =
+    (h*60*60)+(m*60)+s
 
-let digits _ =
-    failwith "Not implemented"
+let toTime s =
+    if s<0 then 0,0,0
+    else
+        let hours = 
+            s/(60*60)
+        let mins = 
+            (s-(hours*60*60))/60
+        hours,mins,s%60
 
-let minmax _ =
-    failwith "Not implemented"
+let digits x =
+    let rec digitcount x acc =
+        match x<10 && -10<x with
+        |true -> (acc+1)
+        |false -> digitcount (x/10) (acc+1) 
+    digitcount x 0
+
+let minmax (_,_,_,_) = 
+    
 
 let isLeap _ =
     failwith "Not implemented"
